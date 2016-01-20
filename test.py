eca2c15a99da88e5c2c0ae842ce95b4195b6e908
Em Lowing <elowing@gmail.com>
2016-01-19 20:36:55 -0500

    Pitchfork returning artist/album pairs

--- test.py
+++ test.py
@@ -1,18 +1,39 @@
-from gmusicapi import Mobileclient
 import os
 
-USER = os.environ.get('GOOGLE_PERSONAL_USER')
-PASS = os.environ.get('GOOGLE_PERSONAL_PASS')
+import gmusicapi
+from lxml import html
+import requests
 
-api = Mobileclient()
-api.login(USER, PASS, Mobileclient.FROM_MAC_ADDRESS)
+USER = os.environ.get('GOOGLE_USER')
+PASS = os.environ.get('GOOGLE_PASS')
 
-# from pdb import set_trace; set_trace()
-library = api.get_all_songs()
-sweet_track_ids = [track['id'] for track in library
-                   if track['artist'] == 'The Cat Empire']
 
-results = api.search_all_access('query')
+def main():
+    pitchfork()
+    google_music()
 
-playlist_id = api.create_playlist('Rad muzak')
-api.add_songs_to_playlist(playlist_id, sweet_track_ids)
+
+def pitchfork():
+    page = requests.get('http://pitchfork.com/best/high-scoring-albums')
+    tree = html.fromstring(page.content)
+
+    artists = tree.xpath('//*[@id="main"]/ul/li/ul/li/a/div[2]/h1/text()')
+    albums = tree.xpath('//*[@id="main"]/ul/li/ul/li/a/div[2]/h2/text()')
+    return zip(artists, albums)
+
+
+def google_music():
+    api = gmusicapi.Mobileclient()
+    api.login(USER, PASS, gmusicapi.Mobileclient.FROM_MAC_ADDRESS)
+
+    library = api.get_all_songs()
+    sweet_track_ids = [track['id'] for track in library
+                       if track['artist'] == 'The Cat Empire']
+
+    # results = api.search_all_access('query')
+
+    playlist_id = api.create_playlist('Rad muzak')
+    api.add_songs_to_playlist(playlist_id, sweet_track_ids)
+
+if __name__ == '__main__':
+    main()
