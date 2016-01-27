f816f34e1e04805cb9e7a7b05d3020314f7c72eb
Em Lowing <elowing@gmail.com>
2016-01-27 17:11:41 -0500

    Add basic files to package extension

--- test.py
+++ test.py
@@ -27,14 +27,13 @@ def google_music(pairs):
     api.login(USER, PASS, gmusicapi.Mobileclient.FROM_MAC_ADDRESS)
     playlists = api.get_all_playlists()
     playlist_id = [playlist['id'] for playlist in playlists
-        if playlist['id'] == '86be30ae-2648-4ba6-8f83-f914ccdd8434']
+        if playlist['id'] == '86be30ae-2648-4ba6-8f83-f914ccdd8434'][0]
 
     for pair in pairs:
         query = ' '.join(pair)
         results = api.search_all_access(query)
         track_ids = [song['track']['storeId'] for song in results['song_hits']]
         if track_ids:
-            from pdb import set_trace; set_trace()
             api.add_songs_to_playlist(playlist_id, track_ids)
 
 if __name__ == '__main__':
